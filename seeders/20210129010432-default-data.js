491066a499542a4804334bfb342ddc528e62e3f8
Allen-TaiLin <skygary2000@gmail.com>
2021-01-29 10:47:34 +0800

    add default seeder

---
+++ seeders/20210129010432-default-data.js
@@ -0,0 +1,48 @@
+'use strict';
+const bcrypt = require('bcryptjs')
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678'
+}
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const userId = await queryInterface.bulkInsert('Users', [{
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: bcrypt.hashSync(SEED_USER.password, bcrypt.genSaltSync(10), null),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {})
+
+    return await queryInterface.bulkInsert('Todos', Array.from({ length: 10 }).map((item, index) => ({
+      name: `name-${index}`,
+      UserId: userId,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    })
+    ), {})
+
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Todos', null, {})
+    return await queryInterface.bulkDelete('Users', null, {})
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
